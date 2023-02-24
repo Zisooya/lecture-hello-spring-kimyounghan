fb0c21a135eb86f9d8c6d80ddbb75e7754a7fbe6
Park Jisoo <coya319@naver.com>
2023-02-25 00:59:22 +0900

    feat(jdbc template): Add JdbcTemplateMemberRepository

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -9,13 +9,11 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
     // 외부에서 넣어주도록 설정
-    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
