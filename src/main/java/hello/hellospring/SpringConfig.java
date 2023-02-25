e9f0b418f33c318b100bee98bb13a4c7b4569503
Park Jisoo <coya319@naver.com>
2023-02-26 00:02:10 +0900

    feat(spring data JPA): Add SpringDataJpaMemberRepository

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -20,22 +20,34 @@ public class SpringConfig {
         this.dataSource = dataSource;
     }
 */
+
+/*
+// JPA 사용 시
     private EntityManager em;
 
     public SpringConfig(EntityManager em) {
         this.em = em;
     }
+*/
+
+    // 이렇게 하면 스프링 데이터 JPA가 구현체를 만들어 놓은 것이 빈으로 등록이 되고 가져다 쓸 수 있음.
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
+
 }
