2fd7774ed3f06df4bad85655a4b6baede7540abf
Park Jisoo <coya319@naver.com>
2023-02-25 19:09:37 +0900

    feat(JPA): Add JpaMemberRepository

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,9 +1,15 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
 
     // 데이터를 구분하기 위한 시스템 저장용 id
+    //IDENTITY : DB가 알아서 아이디값을 생성해주는 전략
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
 
     public Long getId() {
